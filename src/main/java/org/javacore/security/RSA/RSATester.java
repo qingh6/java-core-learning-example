625056ce417ef0136c4b43cda4fcc59924518ec9
qingh6 <qingh_6@163.com>
2018-09-16 22:02:02 +0800

    RSA非对称加密

---
+++ src/main/java/org/javacore/security/RSA/RSATester.java
@@ -0,0 +1,72 @@
+package org.javacore.security.RSA;
+
+import java.util.Map;
+
+/** *//**
+ * <p>
+ * RSA公钥/私钥/签名工具包
+ * </p>
+ * <p>
+ * 罗纳德·李维斯特（Ron [R]ivest）、阿迪·萨莫尔（Adi [S]hamir）和伦纳德·阿德曼（Leonard [A]dleman）
+ * </p>
+ * <p>
+ * 字符串格式的密钥在未在特殊说明情况下都为BASE64编码格式<br/>
+ * 由于非对称加密速度极其缓慢，一般文件不使用它来加密而是使用对称加密，<br/>
+ * 非对称加密算法可以用来对对称加密的密钥加密，这样保证密钥的安全也就保证了数据的安全
+ * </p>
+ *
+ * @author IceWee
+ * @date 2012-4-26
+ * @version 1.0
+ */
+public class RSATester {
+    static String publicKey;
+    static String privateKey;
+
+    static {
+        try {
+            Map<String, Object> keyMap = RSAUtils.genKeyPair();
+            publicKey = RSAUtils.getPublicKey(keyMap);
+            privateKey = RSAUtils.getPrivateKey(keyMap);
+            System.err.println("公钥: \n\r" + publicKey);
+            System.err.println("私钥： \n\r" + privateKey);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        test();
+        testSign();
+    }
+
+    static void test() throws Exception {
+        System.err.println("公钥加密——私钥解密");
+        String source = "这是一行没有任何意义的文字，你看完了等于没看，不是吗？";
+        System.out.println("\r加密前文字：\r\n" + source);
+        byte[] data = source.getBytes();
+        byte[] encodedData = RSAUtils.encryptByPublicKey(data, publicKey);
+        System.out.println("加密后文字：\r\n" + new String(encodedData));
+        byte[] decodedData = RSAUtils.decryptByPrivateKey(encodedData, privateKey);
+        String target = new String(decodedData);
+        System.out.println("解密后文字: \r\n" + target);
+    }
+
+    static void testSign() throws Exception {
+        System.err.println("私钥加密——公钥解密");
+        String source = "这是一行测试RSA数字签名的无意义文字";
+        System.out.println("原文字：\r\n" + source);
+        byte[] data = source.getBytes();
+        byte[] encodedData = RSAUtils.encryptByPrivateKey(data, privateKey);
+        System.out.println("加密后：\r\n" + new String(encodedData));
+        byte[] decodedData = RSAUtils.decryptByPublicKey(encodedData, publicKey);
+        String target = new String(decodedData);
+        System.out.println("解密后: \r\n" + target);
+        System.err.println("私钥签名——公钥验证签名");
+        String sign = RSAUtils.sign(encodedData, privateKey);
+        System.err.println("签名:\r" + sign);
+        boolean status = RSAUtils.verify(encodedData, publicKey, sign);
+        System.err.println("验证结果:\r" + status);
+    }
+
+}
