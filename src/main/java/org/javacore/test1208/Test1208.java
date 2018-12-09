2b260c0638988ff2fb21acce3a058cfa2b9a7e17
qingh6 <qingh_6@163.com>
2018-12-09 14:32:49 +0800

    test 1208 测试

---
+++ src/main/java/org/javacore/test1208/Test1208.java
@@ -0,0 +1,12 @@
+package org.javacore.test1208;
+
+import org.javacore.thread.LiftOff;
+
+/**
+ * Created by lqh on 2018/12/9.
+ */
+public class Test1208 {
+    public static void main(String[] args) {
+        System.out.println("test 1208");
+    }
+}
